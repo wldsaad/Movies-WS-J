594955563ef4eea383db32bfb46a307561ec48a6
Waleed Saad <waleedsaad.dev@gmail.com>
2024-01-04 15:34:40 +0200

    feat(build): project init

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,32 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Waleed Saad on 04/01/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MoviesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
