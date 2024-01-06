54a0cfd6e72d635bad890714ba43e3371adba9bc
Waleed Saad <waleedsaad.dev@gmail.com>
2024-01-06 02:48:29 +0200

    feat(trending-movies): prepend image base url and size to backdrop path

--- Repository/Repository/Mappers/TrendingMoviesDTOMapper.swift
+++ Repository/Repository/Mappers/TrendingMoviesDTOMapper.swift
@@ -23,8 +23,24 @@ struct TrendingMoviesDTOMapper {
     // MARK: - APIs
     
     func callAsFunction() -> TrendingMovies {
-        let movies: [TrendingMovie] = response.results?.compactMap { .init(title: $0.title, image: $0.backdropPath, releaseDate: $0.releaseDate) } ?? []
+        let movies: [TrendingMovie] = response.results?.compactMap { .init(
+            title: $0.title,
+            image: backdropImagePath($0.backdropPath),
+            releaseDate: $0.releaseDate)
+        } ?? []
+        
         let pagination = Pagination(page: response.page, totalPages: response.totalPages, totalResults: response.totalResults)
+        
         return .init(movies: movies, pagination: pagination)
+    }
+    
+    // MARK: - Methods
+    
+    private func backdropImagePath(_ path: String?) -> String {
+        guard let path, !path.isEmpty else { return "" }
+        
+        let base = "https://image.tmdb.org/t/p/"
+        let size = "w300"
+        return #"\#(base)\#(size)\#(path)"#
     }
 }
